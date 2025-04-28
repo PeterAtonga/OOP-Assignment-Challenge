de2d901cfaf48f711524a92cd2e66e063c62ccfe
PeterAtonga <166705423+PeterAtonga@users.noreply.github.com>
2025-04-28 22:12:51 +0300

    I added challenge5.java to show how while loops are used in java

---
+++ challenge5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int secretNumber = 7;
+        int guess;
+
+        System.out.println("Guess the secret number between 1 and 10:");
+
+        guess = scanner.nextInt(); // First guess
+
+        while (guess != secretNumber) { // Loop until correct
+            System.out.println("Wrong! Try again:");
+            guess = scanner.nextInt(); // Ask again
+        }
+
+        System.out.println("Congratulations! You guessed it!");
+
+        scanner.close();
+    }
+}
